008a23893b1d615022d3072f44d4f480d0e72aeb
Amrit Singh <asingh1092@gmail.com>
2021-05-05 21:38:26 -0400

    sorting and comparable

--- src/com/singh/oop2/inheritance/Wolf.java
+++ src/com/singh/oop2/inheritance/Wolf.java
@@ -1,6 +1,8 @@
 package com.singh.oop2.inheritance;
 
-public class Wolf extends Canine {
+import java.util.Arrays;
+
+public class Wolf extends Canine implements Comparable<Wolf> {
 
     protected int rank;
 
@@ -23,8 +25,34 @@ public class Wolf extends Canine {
         System.out.println("lick");
     }
 
+    public int compareTo(Wolf anotherWolf) {
+        return -(rank - anotherWolf.rank);
+    }
+
+    public String toString() {
+        return ("Rank: " + rank + ", Size: " + size);
+    }
+
     public static void main(String[] args) {
         Wolf alpha = new Wolf(17.1, 1);
-        alpha.bark();
+        Wolf puppy = new Wolf(3, 10);
+        System.out.println(alpha.compareTo(puppy));
+
+        Wolf[] pack = {
+                new Wolf(17.1, 2),
+                new Wolf(3, 10),
+                new Wolf(9.2, 7),
+                new Wolf(9.1, 8),
+                new Wolf(17.01, 3),
+                new Wolf(16.2, 1),
+                new Wolf(16, 4),
+                new Wolf(16, 5),
+                new Wolf(10, 6),
+                new Wolf(5, 9),
+        };
+        System.out.println("Unsorted Pack: " + Arrays.toString(pack));
+        Arrays.sort(pack);
+        System.out.println("========================================");
+        System.out.println("Sorted Pack: " + Arrays.toString(pack));
     }
 }
