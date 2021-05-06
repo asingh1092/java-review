e03f4cf165100bbeebb4d70764f080d265f3a8df
Amrit Singh <asingh1092@gmail.com>
2021-05-05 20:53:20 -0400

    car and stuff

---
+++ src/com/singh/oop2/interfaces/Car.java
@@ -0,0 +1,28 @@
+package com.singh.oop2.interfaces;
+
+public class Car {
+    private String make;
+    private String model;
+    private int year;
+    private int charge; // it's an electric car
+    private boolean powered;
+    private int speed;
+
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    public void start() {
+        if (charge > 0) {
+            System.out.println("Ding!");
+            powered = true;
+        }
+    }
+
+    public void groom() {
+        if (speed == 0) {
+            System.out.println("Soap, rinse, wax, and a little tree air freshener.");
+        }
+    }
+}
