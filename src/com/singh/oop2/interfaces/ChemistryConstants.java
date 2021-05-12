2fd03d5a3adf866f115b7a129ecd201b0f4c49ba
Amrit Singh <asingh1092@gmail.com>
2021-05-11 22:13:23 -0400

    constant interface example

---
+++ src/com/singh/oop2/interfaces/ChemistryConstants.java
@@ -0,0 +1,10 @@
+package com.singh.oop2.interfaces;
+
+// This is ane example of a constant interface
+// this is an anti pattern and is advised to not do
+public interface ChemistryConstants {
+    public static final double AVOGADROS_NUMBER = 6.02214199e23;
+    public static final double FARADAY_CONSTANT = 96485.33;
+    public static final double COULOMB_CONSTANT = 8.987551e9;
+    public static final double PLANCK_CONSTANT = 6.62607004e34;
+}
