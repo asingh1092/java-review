189a24ce2b864e250b41894eb1777ed834538b39
Amrit Singh <asingh1092@gmail.com>
2021-05-23 21:42:12 -0400

    array list with autoboxing

---
+++ src/com/singh/oop3/generics/AutoboxingIntArrayListDemo.java
@@ -0,0 +1,21 @@
+package com.singh.oop3.generics;
+
+import java.util.ArrayList;
+
+public class AutoboxingIntArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<Integer> scores = new ArrayList<>();
+        scores.add(90); //auto boxing!
+        scores.add(95);
+        scores.add(110);
+        scores.add(99);
+        scores.add(105);
+        System.out.println(scores.toString());
+
+        int sum = 0;
+        for (Integer score: scores) { //for each loosp works here!
+            sum += score; // autoboxing!
+        }
+        System.out.println("Total points: " + sum);
+    }
+}
