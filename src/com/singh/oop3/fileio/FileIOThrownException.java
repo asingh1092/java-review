639ac4e3de04637090844a08e2c68fee76bbdb46
Amrit Singh <asingh1092@gmail.com>
2021-05-23 21:02:13 -0400

    throws example for filenotfoundexception

---
+++ src/com/singh/oop3/fileio/FileIOThrownException.java
@@ -0,0 +1,28 @@
+package com.singh.oop3.fileio;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileIOThrownException {
+    public static void main(String[] args) throws FileNotFoundException {
+        String fileName = args[0];
+        String word = args[1];
+
+        File file = new File(args[0]);
+        Scanner scan = null;
+
+        scan = new Scanner(file);
+        int lineCount = 0;
+        System.out.printf("Lines in %s containing %s:\n", args[0], args[1]);
+
+        while (scan.hasNextLine()) {
+            String line  = scan.nextLine();
+            if (line.contains(word)) {
+                System.out.println(lineCount + ": " + line);
+            }
+            lineCount++;
+        }
+        scan.close();
+    }
+}
