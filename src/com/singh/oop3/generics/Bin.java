7e63dc4d3fa4c445c3d0a75d43954d985ab35289
Amrit Singh <asingh1092@gmail.com>
2021-05-24 22:15:08 -0400

    generic examples!

---
+++ src/com/singh/oop3/generics/Bin.java
@@ -0,0 +1,30 @@
+package com.singh.oop3.generics;
+
+import com.singh.oop2.inheritance.Car;
+import com.singh.oop2.inheritance.Dog;
+import com.singh.oop2.inheritance.Wolf;
+
+public class Bin {
+
+    private Object content;
+
+    public Bin(Object content) {
+        this.content = content;
+    }
+
+    public Object getContent() {
+        return content;
+    }
+
+    public void setContent(Object content) {
+        this.content = content;
+    }
+
+    public static void main(String[] args) {
+        Bin test = new Bin("I'm a basic string");
+        test.setContent(new Wolf(1, 4));
+        test.setContent(new Dog("Spot", 1));
+        ((Car)test.getContent()).groom();
+        System.out.println(test.getContent());
+    }
+}
---
+++ src/com/singh/oop3/generics/GenericBin.java
@@ -0,0 +1,30 @@
+package com.singh.oop3.generics;
+
+import com.singh.oop2.inheritance.Car;
+
+public class GenericBin<T> {
+
+    private T content;
+
+    public GenericBin(T content) {
+        this.content = content;
+    }
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+
+    public static void main(String[] args) {
+        GenericBin<String> test = new GenericBin<>("I'm a basic String");
+        test.setContent("short and stout");
+        test.setContent("eleemnt is my handle");
+        test.setContent("getContent is my spout");
+        GenericBin<Car> test2 = new GenericBin<Car>(new Car("Yuhina", "Spark", 2037));
+        test2.setContent(new Car("AnotherYuhina", "Spark2", 2050));
+        System.out.println(test2.toString());
+    }
+}
