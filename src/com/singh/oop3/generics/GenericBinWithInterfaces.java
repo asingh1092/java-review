380f062363b0f17e685a8e9d6de512e8eefdfec0
Amrit Singh <asingh1092@gmail.com>
2021-05-24 22:33:48 -0400

    generic class with interface extension

---
+++ src/com/singh/oop3/generics/GenericBinWithInterfaces.java
@@ -0,0 +1,26 @@
+package com.singh.oop3.generics;
+
+import com.singh.oop2.encapsulation.Insect;
+import com.singh.oop2.inheritance.Car;
+
+public class GenericBinWithInterfaces<T extends Insect> {
+
+    private T content;
+
+    public GenericBinWithInterfaces(T content) {
+        this.content = content;
+    }
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+
+    public static void main(String[] args) {
+        GenericBinWithInterfaces<Insect> test = new GenericBinWithInterfaces<>(new Insect(12));
+        System.out.println(test.toString());
+    }
+}
