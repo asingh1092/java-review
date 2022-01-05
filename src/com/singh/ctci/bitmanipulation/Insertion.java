2a615d35579e2e611ce9d2129bbc2b1a831d5898
Amrit Singh <asingh1092@gmail.com>
2022-01-04 21:30:57 -0500

    computing the parity of a word

--- src/com/singh/ctci/bitmanipulation/Insertion.java
+++ src/com/singh/ctci/bitmanipulation/Insertion.java
@@ -1,4 +1,39 @@
 package com.singh.ctci.bitmanipulation;
 
 public class Insertion {
+
+    /*
+     N and M = two 32-bit numbers
+     i and j = two bit positions
+     Write a method to insert M into N such that M starts at bit j and ends at bit i.
+
+     Assume that bits j through i have enough space to fit all of M.
+      i.e if M = 10011, you can assume that there are at least 5 bits between j and i.
+          you would not have j = 3 and i =2 because M could not fully fit between bit 3 and bit 2
+
+     Example:
+          Input: N = 100 0000 0000, M = 10 011, i = 2 , j = 6
+          Output N = 100 0100 1100
+
+          100 0000 0000
+               j    i
+               100 11
+
+          100 0100 1100
+     */
+
+    public static int bitInsert(int n, int m, int i, int j) {
+        int MAX_INT_POSITION = 32;
+        int MIN_INT_POSITION = 0;
+
+        if ((j < i) || (j >= MAX_INT_POSITION) | (i < MIN_INT_POSITION)) {
+            return 0;
+        }
+
+
+
+        m <<= i;
+        return n + m;
+    }
+
 }
---
+++ src/com/singh/epi/ComputingTheParityOfAWord.java
@@ -0,0 +1,35 @@
+package com.singh.epi;
+
+public class ComputingTheParityOfAWord {
+
+    /* The parity of a binary word is 1 if the number of 1s in the word is odd; otherwise it is 0.
+       e.g. the parity of 1011 is 1.
+       e.g. the parity of 100010000 is 0.
+
+       x % 2 == 0 -> even -> return 0
+       x % 2 != 0 -> odd -> return 1
+
+       1. take input number and AND it to 1
+       2. then right shift by 1
+       3. do this until input number is 0
+
+     */
+
+    public static short computeParity(long input) {
+        short countBits = 0;
+        while (input != 0) {
+            countBits += (input & 1);
+            input >>>= 1;
+        }
+
+        if (countBits % 2 == 0)
+            return 0;
+        else
+            return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(computeParity(11));
+        System.out.println(computeParity(272));
+    }
+}
