9910a807b5ed4962d726facfcb43154a0dcbc11e
Amrit Singh <asingh1092@gmail.com>
2022-01-06 21:20:16 -0500

    ctci - 5.1 insertion

--- src/com/singh/ctci/bitmanipulation/Insertion.java
+++ src/com/singh/ctci/bitmanipulation/Insertion.java
@@ -5,33 +5,44 @@ public class Insertion {
     /*
      N and M = two 32-bit numbers
      i and j = two bit positions
+     i = start bit
+     j = end bit
      Write a method to insert M into N such that M starts at bit j and ends at bit i.
 
      Assume that bits j through i have enough space to fit all of M.
       i.e if M = 10011, you can assume that there are at least 5 bits between j and i.
-          you would not have j = 3 and i =2 because M could not fully fit between bit 3 and bit 2
+          you would not have j = 3 and i = 2  because M could not fully fit between bit 3 and bit 2
 
      Example:
-          Input: N = 100 0000 0000, M = 10 011, i = 2 , j = 6
-          Output N = 100 0100 1100
-
-          100 0000 0000
-               j    i
-               100 11
-
-          100 0100 1100
+          Input: N = 10000000000, M = 10 011, i = 2 , j = 6
+          Output N = 10001001100
+          10987654321
+          10000000000
+               j   i
+          00001001100
+          10001001100
      */
 
     public static int bitInsert(int n, int m, int i, int j) {
-        int MAX_INT_POSITION = 32;
-        int MIN_INT_POSITION = 0;
-
-        if ((j < i) || (j >= MAX_INT_POSITION) | (i < MIN_INT_POSITION)) {
-            return 0;
+        int ret = 0;
+        if (j < i || i < 0 || j >= 32) {
+            return ret;
         }
 
+        // make mask to clear i to j in n
+        int allOnes = ~0;
+        int left = j < 31 ? (allOnes << (j + 1)) : 0;
+        int right = ((1 << i) - 1);
+        int mask = left | right;
+
+        // clear n
+        n = n & mask;
+
+        // right shift m by i
         m <<= i;
-        return n + m;
+
+        ret = n | m;
+        return ret;
     }
 
 }
