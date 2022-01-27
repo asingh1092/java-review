0bd8ce287d2c48218910d32bf6f25341b8178277
Amrit Singh <asingh1092@gmail.com>
2022-01-26 20:37:20 -0500

    alt solution to dutchflag partition with more space

--- src/com/singh/epi/arrays/DutchFlagPartition.java
+++ src/com/singh/epi/arrays/DutchFlagPartition.java
@@ -1,7 +1,6 @@
 package com.singh.epi.arrays;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class DutchFlagPartition {
     /* Suppose you're given an Array A with a pivot index i. Rearrange A such
@@ -9,14 +8,51 @@ public class DutchFlagPartition {
 
     public static void dutchFlagPartition(List<Integer> A, int index) {
         int pivot = A.get(index);
-        for (int i = 0; i < A.size() - 1; i++) {
-            if (A.get(i) == pivot) {
-                continue;
-            }
-            if (A.get(i) > A.size()) {
-                Collections.swap(A, i, index);
+        int greaterIndex = A.indexOf(pivot) + 1;
+        System.out.println("Pivot: " + pivot + "\nBefore: " + A);
+        for (int lesserIndex = 0; lesserIndex <= index - 1; lesserIndex++) {
+            if (A.get(lesserIndex) > pivot) {
+               Collections.swap(A, lesserIndex, greaterIndex);
+               greaterIndex++;
+               lesserIndex--;
             }
         }
     }
 
+    public static void dutchFlagPartitionO_n_space(List<Integer> A, int index) {
+        List<Integer> smaller = new ArrayList<>();
+        List<Integer> bigger = new ArrayList<>();
+        List<Integer> pivots = new ArrayList<>();
+        int pivot = A.get(index);
+        System.out.println("Pivot: " + pivot + "\nBefore: " + A);
+        for (Integer n : A) {
+            if (n < pivot) {
+                smaller.add(n);
+            }
+            if (n > pivot) {
+                bigger.add(n);
+            }
+            if (n == pivot) {
+                pivots.add(n);
+            }
+        }
+        smaller.addAll(pivots);
+        smaller.addAll(bigger);
+        A.clear();
+        A.addAll(smaller);
+        System.out.println("Afterwards: " + A);
+        // Space: O(n)
+        // Time: O(n)
+    }
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        List<Integer> aList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            aList.add(rand.nextInt(15));
+        }
+        dutchFlagPartitionO_n_space(aList, rand.nextInt(10));
+    }
+
 }
+
