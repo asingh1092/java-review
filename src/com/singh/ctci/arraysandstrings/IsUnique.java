16f95fc85cd0c4c143d5f230b613b817bde658a5
Amrit Singh <asingh1092@gmail.com>
2022-01-27 19:55:03 -0500

    is unique with sorting and O(nlogn) time

--- src/com/singh/ctci/arraysandstrings/IsUnique.java
+++ src/com/singh/ctci/arraysandstrings/IsUnique.java
@@ -1,11 +1,15 @@
 package com.singh.ctci.arraysandstrings;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class IsUnique {
 
     public static boolean isUnique(String s) {
+        /*
+           n = number of characters = s.length()
+           Space: O(n)
+            Time: O(n)
+         */
         boolean ret = true;
         Set<Character> set = new HashSet<>();
         for (int i = 0; i < s.length(); ++i) {
@@ -16,5 +20,31 @@ public class IsUnique {
             }
         }
         return ret;
+    }
+
+    public static boolean betterIsUnique(String s) {
+        /*
+           Space: O(1)
+           Time: sort first O(nlogn) + look through list O(n)
+                so 0(nlogn)
+         */
+        boolean ret = true;
+        char[] temp = s.toCharArray();
+        Arrays.sort(temp);
+        char prev = temp[0];
+        for (int i = 1; i < temp.length; ++i) {
+            if (temp[i] == prev) {
+                ret = false;
+                break;
+            }
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        String test1 = "thehehehehehe";
+        String test2 = "tyuioplkjhgfdsa";
+        System.out.println(isUnique(test1));
+        System.out.println(isUnique(test2));
     }
 }
