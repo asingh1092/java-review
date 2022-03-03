e2824d898786b452d04d23ef9e9024c43b8f1faa
Amrit Singh <asingh1092@gmail.com>
2022-03-03 18:39:45 -0500

    trial division generation of primes

--- src/com/singh/epi/arrays/EnumerateAllPrimes.java
+++ src/com/singh/epi/arrays/EnumerateAllPrimes.java
@@ -19,7 +19,7 @@ public class EnumerateAllPrimes {
         }
         List<Integer> ret = new ArrayList<>();
         for (int i = 2; i < value + 1; i++) {
-            if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0) {
+            if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
             } else {
                 ret.add(i);
             }
@@ -27,12 +27,35 @@ public class EnumerateAllPrimes {
         return ret;
     }
 
+    public static List<Integer> generatePrimes(int value) {
+        // if less than 2, return empty list
+        if (value < 2) {
+            return Collections.emptyList();
+        }
+        List<Integer> primes = new ArrayList<>();
+        List<Boolean> isPrime = new ArrayList<>(Collections.nCopies(value + 1, true)); // + 1 here is for 0
+        // 0 & 1 are not prime
+        isPrime.set(0, false);
+        isPrime.set(1, false);
+        // start p at 2 because of the above
+        for (int p = 2; p <= value; p++) {
+            if (isPrime.get(p)) {
+                primes.add(p);
+                // Sieve the multiples of p
+                for (int i = p * 2; i <= value; i += p) {
+                    isPrime.set(i, false);
+                }
+            }
+        }
+        return primes;
+    }
+
     public static void main(String[] args) {
-        System.out.println(primeListTo(10));
-        System.out.println(primeListTo(18));
-        System.out.println(primeListTo(19));
-        System.out.println(primeListTo(33));
-        System.out.println(primeListTo(100));
+        System.out.println(generatePrimes(10));
+        System.out.println(generatePrimes(18));
+        System.out.println(generatePrimes(19));
+        System.out.println(generatePrimes(33));
+        System.out.println(generatePrimes(100));
 
     }
 }
