6c464bc54d0f745c7bfdebcdd677fb6c55a2f2a7
Amrit Singh <asingh1092@gmail.com>
2022-03-23 21:17:08 -0400

    singly linked and doubly linked list constructors

--- src/com/singh/epi/linkedlists/ListNode.java
+++ src/com/singh/epi/linkedlists/ListNode.java
@@ -3,9 +3,16 @@ package com.singh.epi.linkedlists;
 public class ListNode<T> {
     private T data;
     private ListNode<T> next;
+    private ListNode<T> prev;
 
     public ListNode(T data, ListNode<T> next) {
         this.data = data;
         this.next = next;
+    }
+
+    public ListNode(T data, ListNode<T> next, ListNode<T> prev) {
+        this.data = data;
+        this.next = next;
+        this.prev = prev;
     }
 }
