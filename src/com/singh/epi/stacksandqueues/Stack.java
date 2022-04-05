21a246c56c80379f28fc24078759de75438aeea5
Amrit Singh <asingh1092@gmail.com>
2022-04-04 20:16:15 -0400

    implemented a stack

--- src/com/singh/epi/stacksandqueues/Stack.java
+++ src/com/singh/epi/stacksandqueues/Stack.java
@@ -1,13 +1,40 @@
 package com.singh.epi.stacksandqueues;
 
+import java.util.NoSuchElementException;
+
 public class Stack<T> {
     private Node<T> top;
+
+    public Stack() {
+    }
 
     public Stack(Node<T> top) {
         this.top = top;
     }
 
+    private void checkIfEmpty() {
+        if (isEmpty()) throw new NoSuchElementException("Empty stack!");
+    }
+
+    public Boolean isEmpty() {
+        return top != null;
+    }
+
     public T peek() {
-        return top
+        checkIfEmpty();
+        return top.getData();
+    }
+
+    public T pop() {
+        checkIfEmpty();
+        T ret = top.getData();
+        top = top.getNext();
+        return ret;
+    }
+
+    public void push(T dataToPush) {
+        Node<T> newData = new Node<>(dataToPush);
+        newData.setNext(top);
+        top = newData;
     }
 }
